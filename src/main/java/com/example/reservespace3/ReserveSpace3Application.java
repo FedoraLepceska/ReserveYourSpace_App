5663f8feaf53a8c2654be8a9a7a7e127d54b930c
flepceska <flepceska@gmail.com>
2022-04-26 20:26:36 +0200

    Initial commit

---
+++ src/main/java/com/example/reservespace3/ReserveSpace3Application.java
@@ -0,0 +1,13 @@
+package com.example.reservespace3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveSpace3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveSpace3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reservespace3/ReserveSpace3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservespace3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveSpace3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
